18e66f4525b7618b290b1b073d6a07e3564fe5d9
Tony Ngo <contact@suas.com>
2021-03-07 21:25:02 -0500

    style and rephrase

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,12 +5,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
   let more_skills_expanded = false;
 
   let handleExpandCollapse = () => {
+    const numberOfIcons = document.querySelectorAll('#skills article').length;
+    const unitWidth = 110;
+    const unitHeight = unitWidth;
+    const margin = () => {
+      if(window.innerWidth > 1680) return 6 * 16;
+      if(window.innerWidth > 1280) return 5 * 16;
+      if(window.innerWidth > 736) return 4 * 16;
+      return 2 * 16;
+    }
+
+    const numerOfIconsPerRow = Math.floor((window.innerWidth - 2 * margin()) / unitWidth);
+    const numberOfRowsNeeded = Math.ceil(numberOfIcons/numerOfIconsPerRow);
+    const collapsedTotalHeight = 2 * unitHeight + 0;
+    const expandedTotalHeight = numberOfRowsNeeded * unitHeight + 0;
+    console.log('numerOfIconsPerRow', numerOfIconsPerRow, numberOfIcons, numberOfRowsNeeded);
+    console.log('window.innerWidth', window.innerWidth);
     if (more_skills_expanded) {
-      skills_div.classList.add('expanded');
+      // skills_div.classList.add('expanded');
+      skills_div.style.height = `${expandedTotalHeight}px`;
       more_skills.classList.add('up');
       more_skills.classList.remove('down');
     } else {
-      skills_div.classList.remove('expanded');
+      // skills_div.classList.remove('expanded');
+      skills_div.style.height = `${collapsedTotalHeight}px`;
       more_skills.classList.add('down');
       more_skills.classList.remove('up');
     }
