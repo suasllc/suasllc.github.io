de274dfcd511cb050119e5fa067985a7a0af054b
Tony Ngo <contact@suas.com>
2021-03-14 20:03:08 -0400

    skill div always expanded to fit content

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -87,9 +87,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // }, interval);
   });
 
-  if (skills_div) skills_div.style.height = `${collapsedTotalHeight()}px`;
+  // if (skills_div) skills_div.style.height = `${collapsedTotalHeight()}px`;
 
   let handleExpandCollapse = () => {
+    return; // I don't need it right now
     if (more_skills_expanded) {
       skills_div.style.height = `${expandedTotalHeight()}px`;
       more_skills.classList.add('up');
@@ -161,41 +162,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
     })
   });
 
-  more_skills.addEventListener('click', e => {
-    more_skills_expanded = !more_skills_expanded;
-    handleExpandCollapse();
-  });
+  // more_skills.addEventListener('click', e => {
+  //   more_skills_expanded = !more_skills_expanded;
+  //   handleExpandCollapse();
+  // });
 
   const isMobile = () => /iPhone|iPad|iPod|Android|Windows Phone|webOS|BlackBerry/i.test(navigator.userAgent);
   const isAndroid = () => /Android/i.test(navigator.userAgent);
 
-  if (isMobile()) {
-    skills_div.addEventListener('click', e => {
-      setTimeout(() => {
-        more_skills_expanded = !more_skills_expanded;
-        handleExpandCollapse();
-      }, 100);
-    });
-  } else {
-    skills_div.addEventListener('mouseover', e => {
-      setTimeout(() => {
-        if (!more_skills_expanded) {
-          more_skills_expanded = true;
-          skills_div.classList.add('show_over_flow');
-          handleExpandCollapse();
-        }
-      }, 100);
-    });
-    skills_div.addEventListener('mouseleave', e => {
-      setTimeout(() => {
-        if (more_skills_expanded) {
-          more_skills_expanded = false;
-          skills_div.classList.remove('show_over_flow');
-          handleExpandCollapse();
-        }
-      }, 5000);
-    });
-  }
+  // if (isMobile()) {
+  //   skills_div.addEventListener('click', e => {
+  //     setTimeout(() => {
+  //       more_skills_expanded = !more_skills_expanded;
+  //       handleExpandCollapse();
+  //     }, 100);
+  //   });
+  // } else {
+  //   skills_div.addEventListener('mouseover', e => {
+  //     setTimeout(() => {
+  //       if (!more_skills_expanded) {
+  //         more_skills_expanded = true;
+  //         skills_div.classList.add('show_over_flow');
+  //         handleExpandCollapse();
+  //       }
+  //     }, 100);
+  //   });
+  //   skills_div.addEventListener('mouseleave', e => {
+  //     setTimeout(() => {
+  //       if (more_skills_expanded) {
+  //         more_skills_expanded = false;
+  //         skills_div.classList.remove('show_over_flow');
+  //         handleExpandCollapse();
+  //       }
+  //     }, 5000);
+  //   });
+  // }
   back_to_top_buttons.forEach(btt => {
     btt.addEventListener('click', e => {
       // window.scrollTo(0, 0);
